2bf53e2fc5354c80cdb0f599869c244e472a0298
Shanzeh Asif <shanzehasif@Shanzehs-MacBook-Pro.local>
2022-06-02 06:42:01 +0500

    2nd commit

--- src/Project/GroupProject01.java
+++ src/Project/GroupProject01.java
@@ -5,6 +5,18 @@ public class GroupProject01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a=0;
+		int b=1;
+		int c;
+		
+		for (int i=0; i<10; i++) {
+			
+			System.out.print(a+" ");  
+			c=a+b;
+			a=b;
+			b=c;
+	}
+	}
 	}
 
-}
+
